0d1406d39344566ec7dd8ccc0e3cf233565e248d
Liang-Chi Hsieh <viirya@gmail.com>
2021-10-21 23:24:44 -0700

    init import.

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use kube::{Client, api::{Api, DynamicObject}, discovery, ResourceExt};
+
+#[tokio::main]
+async fn main() -> Result<(), kube::Error> {
+  let client = Client::try_default().await?;
+  let apigroup = discovery::group(&client, "ais-sre.apple.com/v1beta1").await?;
+  let (ar, caps) = apigroup.recommended_kind("NarrativeIssuer").unwrap();
+  let api: Api<DynamicObject> = Api::all_with(client.clone(), &ar);
+  for service in api.list(&Default::default()).await? {
+      println!("Found APIService: {}", service.name());
+  }
+  Ok(())
+}
